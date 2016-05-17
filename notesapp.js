0612e49a220d7fd1cf16bb8cc6958774bdb2793f
demiladebamgbose <demiladebamgbose94@gmail.com>
2016-05-17 16:25:35 +0100

    add listNotes function

--- notesapp.js
+++ notesapp.js
@@ -4,5 +4,13 @@ function NotesApplication(author){
 	this.create= function(note_content){
 		this.note.push(note_content);
 	}
+	this.listNotes = function(){
+		var string;
+		for(var i =0; i < this.note.length; i++){
+			string='';
+			string += 'Note ID: '+ i +'\n' + this.note[i] + '\n' + 'By Author' + this.author;
+			console.log(string);
+		}
+	}
 
 }
